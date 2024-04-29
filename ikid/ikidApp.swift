d1d04ccf1a4c48eac4abf1685e60af710e6e297a
BonieXCW <boniewang77701339@gmail.com>
2024-04-29 12:33:31 -0700

    Initial Commit

---
+++ ikid/ikidApp.swift
@@ -0,0 +1,17 @@
+//
+//  ikidApp.swift
+//  ikid
+//
+//  Created by stlp on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ikidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

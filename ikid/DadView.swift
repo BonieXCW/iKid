9583c34e4df6f46a7b6b6a68bacaa46a12ffbf4a
BonieXCW <boniewang77701339@gmail.com>
2024-04-29 19:58:46 -0700

    Finished iKid

---
+++ ikid/DadView.swift
@@ -0,0 +1,18 @@
+//
+//  DadView.swift
+//  ikid
+//
+//  Created by stlp on 4/29/24.
+//
+
+import SwiftUI
+
+struct DadView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DadView()
+}
---
+++ ikid/GoodView.swift
@@ -0,0 +1,18 @@
+//
+//  GoodView.swift
+//  ikid
+//
+//  Created by stlp on 4/29/24.
+//
+
+import SwiftUI
+
+struct GoodView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GoodView()
+}
